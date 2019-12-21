b159d99b98cba35af05452ef506aef6e732e6ac3
Tom Clarke <tomclake96@hotmail.co.uk>
2019-12-21 16:18:44 +0000

    Adding scripts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IPMonitor
-Simple. IP monitor that uses msmtp to email my IP address
+Simple IP monitor that uses msmtp to email my IP address.
+
+In crontab,to have it run every 30 mins:
+```
+*/30 * * * * cd /home/tom/Documents/IPMonitor; ./email_ip.sh
+```
---
+++ email_ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Gets the internet IP according to the PI and emails it using MSMTP and the email_template.txt
+
+date=$(date +"%m-%d-%YT%T")
+ip=$(./getip.sh)
+
+cp email_template.txt email_working.txt
+
+sed -i -e 's/%IP%/'${ip}'/g' -e 's/%DATE%/'${date}'/g' email_working.txt
+
+cat email_working.txt | msmtp tomclarke96@hotmail.co.uk
